565347444c5e47abe2f7280ba6e0770098242eb2
Afells <Akellofells62@gmail.com>
2024-01-27 15:35:40 -0500

    started Interact system

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 public partial class player : CharacterBody2D
 {
+	[Export] public Area2D interactionArea;
+	[Export] public Area2D interactable;
 	[Export] public float speed = 300;
 
 
@@ -11,7 +15,37 @@ public partial class player : CharacterBody2D
 		Vector2 moveInput = Input.GetVector("left", "right", "up", "down");
 
 		Velocity = moveInput * speed;
-
+		//GD.Print("working");
 		MoveAndSlide();
+
 	}
+	
+
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionJustPressed("interact"))
+		{
+			Interact();
+		}
+
+	   
+	}
+
+	void Interact()
+	{
+		interactable.Call("Use");
+	}
+
+
+
+private void _on_area_2d_area_entered(Area2D area)
+{
+	if(area.IsInGroup("Interactable"))
+		{
+			interactable = area;
+			GD.Print(interactable);
+		}
+	
+	// Replace with function body.
+}
 }
