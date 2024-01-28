1f0a21128e504e848299e800e930bcb675d1c823
Osaze Shears <yoze15@gmail.com>
2024-01-28 14:17:44 -0500

    Added main menu logic

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -10,21 +10,38 @@ public partial class GameManager : Node2D
 	[Export] public Node2D[] Microgames;
 	[Export] public float StartTimer;
 	[Export] public float TaskTimer;
+
+	AudioStreamPlayer audioPlayer = null;
+
+	public bool inMainMenu = true;
 	
 	
 	public override void _Ready()
 	{
 		TimerLoop();
-		/*var rng = new RandomNumberGenerator();
+        /*var rng = new RandomNumberGenerator();
 		instance = this;
 		rng.Randomize();
 		TaskTimer = rng.RandfRange(5f, 7f);
 		GD.Print(TaskTimer);*/
-		
-		//TaskTimer = Random.Range(5,7)
+
+        //TaskTimer = Random.Range(5,7)
+
+        Godot.Collections.Array<Node> children = this.GetChildren();
+
+        foreach (var child in children)
+        {
+            if (child.Name == "AudioStreamPlayer")
+            {
+                audioPlayer = (AudioStreamPlayer) child;
+				// audioPlayer.Play();
+            }
+        }
+    
 	}
 	public override void _Process(double delta)
 	{
+		
 		/*if(TaskTimer>=0)
 		{
 			GD.Print(TaskTimer);
@@ -42,6 +59,16 @@ public partial class GameManager : Node2D
 			await ToSignal(GetTree().CreateTimer(TaskTimer), "timeout");
 			GD.Print("kioui");
 		}
+	}
+
+	public void setInMainMenu(bool inMainMenu)
+	{
+		this.inMainMenu = inMainMenu;
+		if (!inMainMenu)
+		{
+            audioPlayer.Play();
+			audioPlayer.Autoplay = true;
+        }
 	}
 		
 	/*public void PlayMicrogame(int GameNumber)
--- scripts/StartGameButton.cs
+++ scripts/StartGameButton.cs
@@ -5,9 +5,13 @@ public partial class StartGameButton : TextureButton
 {
     AudioStreamPlayer audioPlayer = null;
 
+    private TreeSceneManager treeSceneManager;
+
     bool playerAtMainMenu = true;
     public override void _Ready()
     {
+        GetTreeSceneManager();
+
         Godot.Collections.Array<Node> children = this.GetChildren();
 
         foreach (var child in children)
@@ -43,12 +47,37 @@ public partial class StartGameButton : TextureButton
         audioPlayer.Stream = GD.Load<AudioStream>("res://sounds/Non Micro Game Sounds/UI Sounds/UISelect.wav");
         audioPlayer.Play();
 
-        PackedScene setScene = GD.Load<PackedScene>("res://scenes/set.tscn");
-        Node2D setSceneNode2D = (Node2D) setScene.Instantiate();
-        Node2D parentNode = (Node2D) this.GetParent();
-        parentNode.GlobalPosition = new Vector2(0, 0);
-        parentNode.Visible = false;
-        parentNode.AddChild(setSceneNode2D);
+        //PackedScene setScene = GD.Load<PackedScene>("res://scenes/set.tscn");
+        //Node2D setSceneNode2D = (Node2D) setScene.Instantiate();
+        //Node2D parentNode = (Node2D) this.GetParent();
+        //parentNode.GlobalPosition = new Vector2(0, 0);
+        //parentNode.Visible = false;
+        //parentNode.AddChild(setSceneNode2D);
+        endScene();
+    }
+
+    private void endScene()
+    {
+        if (treeSceneManager != null)
+        {
+            treeSceneManager.setInMainMenu(false);
+            treeSceneManager.BackToMainScene();
+        }
+            
+        QueueFree();
+    }
+
+    private void GetTreeSceneManager()
+    {
+        Node temp = GetParent();
+        if (temp != null)
+            temp = temp.GetParent();
+        if (temp != null)
+            temp = temp.GetParent();
+        if (temp != null)
+            temp = temp.GetParent();
+        if (temp != null)
+            treeSceneManager = (TreeSceneManager)temp;
     }
 }
 
